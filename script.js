9d2c42f3bc75359a8d5885ab5178ba969d1ebd1c
Roberts15 <roberts.berzins25@gmail.com>
2022-03-07 16:02:42 +0200

    darbojas

--- script.js
+++ script.js
@@ -22,27 +22,62 @@ function perimetrs(m1, m2, m3) {
     return (p);
 }
 function pusper(m1, m2, m3) {
-    const pusper = perimetrs(m1, m2, m3) / 2
-    console.log(pusper);
-    return (pusper);
+    const ps = perimetrs(m1, m2, m3)/2
+    console.log(ps);
+    return (ps); 
 }
 function laukums(m1, m2, m3) {
-    const pusper = perimetrs(m1, m2, m3) / 2
-    let ts = Math.sqrt(pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));
+    const n = pusper(m1, m2, m3)
+    console.log(n);
+    let ts = Math.sqrt(n * ((n - m1) * (n - m2) * (n - m3)));
     console.log(ts);
-    return (ts);
-}/*
+    return ts;
+}
 function irTrijsturis(m1, m2, m3) {
-    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) { return true; } else { return false; }
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) { return true; }
+    else { return false; 
+    }
 }
 function rezultats(m1, m2, m3) {
     const m = nolasa()
-    m = m.m1; m2 = m.m2; m3 = m.m3;
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
     console.log(m1, m2, m3);
     if (!nolasa()) {
-        t = "Nekorekti ievaddati, trijstūra malu vērībām jābūt lielākām par nulli"
+        t = "Nekorekti ievadīti dati, trijstūra malu vērtībām jābūt lielākām par nulli. "
     }
-}*/
+    else {
+        if (irTrijsturis(m1, m2, m3)==false){
+            t="Trijstūris neekistē, jo 2 malu garummu summai jābūt lielākai par trešās malas garumu. "
+        }
+        else {
+            t="Trijstūris eksistē. "
+            if (m1==m2&&m2==m3){
+                t+="Tas ir vienādmalu trijstūris. "
+            }
+            else {
+                if (m1==m2||m2==m3||m3==m1) {
+                    t+="Tas ir vienādsānu trijstūris. "
+                }
+            }
+            const p = perimetrs(m1, m2, m3)
+            const s = Math.round( laukums(m1, m2, m3)*100)/100;
+            t+="Perimetrs ir "+p+" un laukums ir "+s+"."
+        }
+    }
+    /*console.log(t)*/
+    return (t)
+}
+function izvadaTekstu(){
+    const teksts = rezultats();
+    console.log(teksts)
+    const sakne = document.getElementById("izvade");
+    const raksti = document.createElement("p");
+    raksti.innerHTML - teksts 
+    sakne.appendChild(raksti);
+}
+
+
+
 
 
 
